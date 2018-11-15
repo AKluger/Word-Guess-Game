3edc9cbc2db12f6a75c02e42ee5d14fa7ad8edaf
Alex <aikluger@gmail.com>
2018-11-14 23:44:54 -0500

    4h Fixed spacing implement win counter and game solved state

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,8 @@
 // Declare Variables for lettersGuessed, numOfGuesses, numOfWins, currentWord
 //equal to getElementbyID named in html body to be used later, could also use ID to change 
 //heading/clue for new word.  Like "if currentWord = toast put up h3 when currentWord=currentGuesses"
-
-var currentWord =[];
+var userGuess;
+var currentWord = [];
 var word;
 var wins = 0;
 var numOfGuesses = 10;
@@ -13,62 +13,70 @@ var wordList = ["bagel", "croissant", "muffin", "flour", "dough"];
 var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
 // CONVERSELY, we could just do each word manually, as in: if userguess = 'b', bakery[0]=userguess instead of _ and do this for each letter?
 
-function newWord()  {
+function doneYet()  {
+  
+    return currentWord.join("") === randomWord;
+    
+  }
+  
+
+
+
+
+//function to generate new round of game
+
     for (var i=0; i<randomWord.length; i++)   {
-        currentWord[i] = " _";
+        currentWord.push(" _ "); 
+        }
+    
+    function guessWord(userGuess)    {
+        for (var i = 0; i < randomWord.length; i++)   {
+            
+            if (randomWord[i] === userGuess) {
+    
+                currentWord[i] = userGuess;
+                
+            }
+            
+    
+            // else if (randomWord[i]!==userGuess)   {
+                // currentWord[i] = " _";
+            // }
         }
 
-    word = currentWord.join(" ");
-    return word;
-}
+        lettersGuessed.push(userGuess);
+        
+        if (doneYet())  {
+           currentWord = currentWord.join("");
+            wins++;
+        }
 
+        else (numOfGuesses--)
+            
+        
+        if (numOfGuesses===0)   {
+            alert("You Lose!");
+        }
+
+         }     // currentWord[i] = " _";
+            // return currentWord;
+    
+            // if (randomWord[i]!==userGuess)  {
+                
 // This function is run whenever the user presses a key.
  document.onkeyup = function(event)  { 
 
-//assign currentWord
-  
-  
 
  //get userGuess and assign variable
   var userGuess = event.key;
-
-  //basically I want to create a funciton that checks if character matches guess, then I want to call that function for each character.
-
-  //or we could copy cargame, if guess=b current word [0] = userGuess or a or g or e or l
-   // so first we're adding userGuess to currentGuesses if not already there or in currentWord, and counting down guesses with g--.  THEN we are checking guess against currentWord.
-   
-    for (var i=0; i<randomWord.length; i++)   {
-
-        if (randomWord[i] === userGuess) {
-
-            currentWord[i] === userGuess;
-            
-        }
-
-        lettersGuessed.push(" "+userGuess);
-        numOfGuesses--;
-         }
-        
-//else if ( userGuess !== l for l== lettersGuessed.charAt(l))  {
-// lettersGuessed.push(userGuess)
  
-  
-// .join can connect the word string when game has been solved.
-// assign currentWord as an array. Display as an array of "_ " for [l] in currentWord if [l] is in lettersGuessed then return l otherwise "_ ". with getElementbyID
-//something like if letter does not appear in word
-//use str search, assigning new variable, if var is >-1, userGuess appears in currentWord.  So how do we return the filled in letter plus empty spaces?
-
-
-//if currentWord exists in currentGuesses then wins++ and move to next round.  If numOfGuesses.length>9 then also go to next word.  Next word is going to be a whole new page?  How do we keep # of wins throughout, with getElementbyID?
-//
-
-//if None of the currentWord array are blanks then win++ and move on to next word
+  guessWord(userGuess);
 
 document.getElementById("wins").innerHTML = wins;
-document.getElementById("word").innerHTML = newWord();
+document.getElementById("word").innerHTML = currentWord;
 document.getElementById("guesses").innerHTML = lettersGuessed;
 document.getElementById("p2").innerHTML = numOfGuesses;
-document.getElementById("guesses").innerHTML = lettersGuessed;
+
 
 
 };
